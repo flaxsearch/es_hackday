6402edbfe266a33cece850ae6f187e174bb59779
Tomáš Fejfar <tomas.fejfar@gmail.com>
2014-09-26 11:59:05 +0100

    wip

--- uk_crimes/app.js
+++ uk_crimes/app.js
@@ -1,10 +1,39 @@
 $(document).ready(function(){
-    $.ajax(
-        'http://84.40.61.82:9200/ukcrimes/_search?query=car',
-    {
-        success: function (data) {
-            $('body').append(data.hits.total);
+    //$('#search').submit(function () {
+        //var query = $(this).find('#q').val();
+    var query = {
+        "query" : {
+            "match_all" : {}
+        },
+        "aggregations" : {
+            "force" : {
+                "terms" : {
+                    "field" : "force"
+                }
+            }
         }
-    }
-    )
+    };
+        $.ajax(
+        'http://84.40.61.82:9200/ukcrimes/_search',
+        {
+            //contentType: 'application/json',
+            data: JSON.stringify(query),
+            dataType : 'json',
+            type: 'post',
+            crossDomain: true,
+
+    //
+            // method: 'post',
+            success: function (data) {
+                console.log(data);
+                $('#container').html('<div><ul id="results"></ul></div>');
+                $.each(data.aggregations.force, function(index, item) {
+                    $('#results').append('<li>' + item.key + ': ' + item.doc_count + '</li>');
+                });
+            }
+        }
+        );
+      //  return false;
+    //});
+
 });
